322ceb8dc1692dd5f19cac073479def98c66b8b0
bamafra <bruna_a_mafra@estudante.sesisenai.org.br>
2022-08-31 16:23:36 -0300

    Initial commit

---
+++ src/Filosofo.java
@@ -0,0 +1,39 @@
+public class Filosofo extends Thread {
+    final static int TEMPO_MAXIMO = 100;
+    Mesa mesa; // garfos -> controle do semafaro (quanto 0 garfo ta disponivel ou não)
+    int filosofo;
+
+    public Filosofo(String nome, Mesa mesaDeJantar, int fil) {
+        super(nome);
+        mesa = mesaDeJantar;
+        filosofo = fil;
+    }
+
+    public void run() { // inicia execução da thread
+        int tempo = 0;
+        while (true) {
+            tempo = (int) (Math.random() * TEMPO_MAXIMO);
+            pensar(tempo);
+            getGarfos(); // usar recurso (garfo) compartilhado
+            tempo = (int) (Math.random() * TEMPO_MAXIMO);
+            comer(tempo);
+            returnGarfos(); // devolve recurso (garfo)
+        }
+    }
+
+    public void pensar(int tempo) {
+        try {
+            sleep(tempo);
+        } catch (InterruptedException e) {
+            System.out.println("O Filófoso pensou em demasia"); // "morreu de fome!"
+        }
+    }
+
+    public void comer(int tempo) {
+        try {
+            sleep(tempo);
+        } catch (InterruptedException e) {
+            System.out.println("O Filósofo comeu em demasia");
+        }
+    }
+}
---
+++ src/Mesa.java
@@ -0,0 +1,28 @@
+public class Mesa {
+    // estrutura de dados: vetor, ArrayList
+    // garfos[5]
+    //filosofos[5] -> int
+
+    boolean[] garfos = new boolean[5];
+
+    public void aDireita(){
+        // Retorna o filosofo a direita
+    }
+
+    public void aEsquerda(){
+        // Retorna o filosofo a esquerda
+    }
+
+    public void garfoEsquerdo(){
+        // Retorna o garfo a direita
+    }
+
+    public void garfoDireito(){
+        // Retorna o garfo a esquerda
+    }
+
+    public void imprimeEstadoFilosofo(){
+        // Imprime o estado de cada filosofo
+        // Comer, pensar (açoes); com fome (esperando)
+    }
+}
